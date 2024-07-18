e353c7b767493cdd9e39d14ff33e10acccb454b0
amandalimon <amandalimon@outlook.com>
2024-07-18 09:33:06 -0600

    axios / react query

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useQuery } from 'react-query'
+import axios from 'axios'
 import Layout from '@components/Layout/Layout'
-import { Card } from 'semantic-ui-react'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
+import { Card } from 'semantic-ui-react'
+
+const query = `
+  query{
+    avos{
+      id
+      image
+      name
+      createdAt
+      sku
+      price
+      attributes {
+        description
+        taste
+        shape
+        hardiness
+      }
+    }
+  }
+`
+
+const baseURL = process.env.NEXT_PUBLIC_SERVICE_URL || 'http://localhost:3000/'
+
+const requester = axios.create({
+  baseURL: baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+// const requester = (endpoint?: string, data?: Record<string, number | string>) =>
+//   fetch(`${baseURL}${endpoint}`, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(data),
+//   })
+
+const useAvocados = () => {
+  return useQuery('avodcados', async () => {
+    const response = await requester.post<{ data: TProduct[] }>('/graphql', { query })
+    return response.data.data
+  })
+
+
+  // const [data, setData] = useState<TProduct[]>([])
+  // const [status, setStatus] = useState<
+  //   'success' | 'loading' | 'error' | 'idle'
+  // >('idle')
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     setStatus('loading')
+  //     try {
+  //       const response = await requester('/graphql', { query })
+
+  //       const { data } = (await response.json()) as { data: TProduct[] }
+  //       setData(data)
+  //       setStatus('success')
+  //     } catch (e) {
+  //       setStatus('error')
+  //       console.log('Something went wrong', e)
+  //     }
+  //   }
+  //   fetchData()
+  // }, [])
+
+  // return {
+  //   data,
+  //   status,
+  // }
+}
 
 const HomePage = () => {
+
+  const { data, status } = useAvocados()
+
+  console.log({ data, status })
+
   return (
     <Layout title="Home">
       <KawaiiHeader />
